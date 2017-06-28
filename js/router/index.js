0f379ac43f6db509a094e97c42128fefb6392025
I809764 <thomas.jung@sap.com>
2017-06-28 14:23:08 +0200

    Node.js Exercise 1

--- js/router/index.js
+++ js/router/index.js
@@ -2,4 +2,6 @@
 
 module.exports = function(app,server) {
 		 app.use("/node", require("./routes/node")());
+		 app.use("/node/ex1", require("./routes/ex1")());
+		 app.use("/node/ex2", require("./routes/ex2")());		 
 };
