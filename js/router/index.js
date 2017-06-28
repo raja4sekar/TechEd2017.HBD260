769592cbb4fca7ba6417276c837875350ac414a8
I809764 <thomas.jung@sap.com>
2017-06-28 16:38:53 +0200

    Async Exercises

--- js/router/index.js
+++ js/router/index.js
@@ -4,5 +4,6 @@ module.exports = function(app,server) {
 		 app.use("/node", require("./routes/node")());
 		 app.use("/node/ex1", require("./routes/ex1")());
 		 app.use("/node/ex2", require("./routes/ex2")());	
-		 app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());		 
+		 app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());	
+		 app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
 };
