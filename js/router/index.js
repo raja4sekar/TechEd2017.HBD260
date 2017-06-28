918fd1f4a1dfa864305f801cf533b308f8159377
I809764 <thomas.jung@sap.com>
2017-06-28 16:04:23 +0200

    JavaScript Basics as Node.js

--- js/router/index.js
+++ js/router/index.js
@@ -3,5 +3,6 @@
 module.exports = function(app,server) {
 		 app.use("/node", require("./routes/node")());
 		 app.use("/node/ex1", require("./routes/ex1")());
-		 app.use("/node/ex2", require("./routes/ex2")());		 
+		 app.use("/node/ex2", require("./routes/ex2")());	
+		 app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());		 
 };
