0234bea3fbc3266c5a3e47700a29156ae87aee28
I809764 <thomas.jung@sap.com>
2017-06-28 16:45:41 +0200

    Text Bundles Exercise

--- js/router/index.js
+++ js/router/index.js
@@ -1,9 +1,13 @@
 "use strict";
 
-module.exports = function(app,server) {
-		 app.use("/node", require("./routes/node")());
-		 app.use("/node/ex1", require("./routes/ex1")());
-		 app.use("/node/ex2", require("./routes/ex2")());	
-		 app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());	
-		 app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
+module.exports = function(app, server) {
+	app.use("/node", require("./routes/node")());
+	app.use("/node/ex1", require("./routes/ex1")());
+	app.use("/node/ex2", require("./routes/ex2")());
+	app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());
+	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
+	app.use("/node/textBundle", require("./routes/textBundle")());
+	app.use("/node/excel", require("./routes/excel")());
+	app.use("/node/xml", require("./routes/xml")());
+	app.use("/node/zip", require("./routes/zip")());
 };
